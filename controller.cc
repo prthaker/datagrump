a98be5d95f5b2b023e7cee74433dfcb4bb142082
Ubuntu <ubuntu@ip-10-194-246-8.ec2.internal>
2013-07-31 06:45:00 +0000

    trying ewma-type things

--- controller.cc
+++ controller.cc
@@ -8,28 +8,35 @@ using namespace Network;
 
 const double alpha = 2;
 const double beta = 2;
-const double rtt_weight = 0.75;
+const double rtt_weight = 0.7;
+const double throughput_weight = 0.7;
+const uint64_t tick_time = 100; // 100 ms between updates
 
 double aimd_wind; //in packets
 double rtt; //in ms
+double curr_throughput;
+
+uint64_t last_seq_num;
+uint64_t last_packet_time;
 
 /* Default constructor */
 Controller::Controller( const bool debug )
   : debug_( debug )
 {
-  aimd_wind = 2;
   rtt = 100; //initial rtt estimate
+  last_seq_num = 0;
+  last_packet_time = 0;
+  curr_throughput = 0.4; // some reasonable starting estimate
 }
 
 /* Get current window size, in packets */
 unsigned int Controller::window_size( void )
 {
-  /* Default: fixed window size of one outstanding packet */
-  int the_window_size = (int)aimd_wind;
+  int the_window_size = (int)(tick_time * curr_throughput);
 
   if ( debug_ ) {
-    fprintf( stderr, "At time %lu, return window_size = %d.\n",
-	     timestamp(), the_window_size );
+    fprintf( stderr, "At time %lu, rtt %f, throughput %f, return window_size = %d.\n",
+	     timestamp(), rtt, curr_throughput, the_window_size );
   }
 
   return the_window_size;
@@ -58,18 +65,23 @@ void Controller::ack_received( const uint64_t sequence_number_acked,
 			       const uint64_t timestamp_ack_received )
                                /* when the ack was received (by sender) */
 {
-  uint64_t new_rtt = timestamp_ack_received - recv_timestamp_acked;
-  
-  if(new_rtt > rtt){
-    aimd_wind = aimd_wind/beta;
-  } else {
-    aimd_wind = aimd_wind + alpha;
-  }
-
-  aimd_wind = std::max(1.0, aimd_wind);
-
+  uint64_t new_rtt = timestamp_ack_received - send_timestamp_acked;
   rtt = (rtt_weight * rtt) + (1-rtt_weight)*(new_rtt);
 
+  uint64_t wait_time = timestamp_ack_received - last_packet_time;
+  if (wait_time >= tick_time) {
+    uint64_t packets_passed = sequence_number_acked - last_seq_num;
+    double ewma_throughput = (throughput_weight*curr_throughput) + 
+      (1 - throughput_weight)*((float)packets_passed/wait_time);
+
+    // in case we get less than 1 packet sent
+    curr_throughput = std::max(1.0/rtt, ewma_throughput); 
+    fprintf(stderr, "new rtt %lu rtt %f wait time %lu ewma throughput %f curr throughput %f packets passed %lu\n", new_rtt, rtt, wait_time, ewma_throughput, curr_throughput, packets_passed);
+
+    last_seq_num = sequence_number_acked;
+    last_packet_time = timestamp_ack_received;
+  }
+  
   if ( debug_ ) {
     fprintf( stderr, "At time %lu, received ACK for packet %lu",
 	     timestamp_ack_received, sequence_number_acked );
